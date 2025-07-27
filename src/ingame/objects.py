d583bc5245a26c54badb55df487a6d4b3d4d7613
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-07-27 18:01:34 -0400

    Update objects.py
    IMPORTANT: Fixed fatal error

--- src/ingame/objects.py
+++ src/ingame/objects.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+from typing import Optional
 
 def Button(
     screen_obj = None,
+    packargs: Optional[dict] = None,
     **kwargs
 ) -> None:
+    if packargs is None:
+        packargs = {}
     if screen_obj is None:
         raise TypeError("Parameter \"screen_obj\" must be specified.")
-    return tk.Button(screen_obj.root, **kwargs).pack(**kwargs)
+    return tk.Button(screen_obj.root, **kwargs).pack(**packargs)
