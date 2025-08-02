1bc853dbc4935a3891cc83344639d63855047a33
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-08-02 13:47:41 -0400

    Update objects.py

--- src/ingame/objects.py
+++ src/ingame/objects.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from typing import Optional, Any
 
 class Button:
+    btn_obj: tk.Button
+
     def __init__(
         self,
         screen_obj: Optional[Any] = None,
@@ -15,4 +17,9 @@ class Button:
         if packargs is None:
             packargs = {}
 
-        tk.Button(screen_obj.root, **kwargs).pack(**{k: v for k, v in packargs.items() if v is not None})
+        self.btn_obj = tk.Button(screen_obj.root, **kwargs)
+        self.btn_obj.pack(**{k: v for k, v in packargs.items() if v is not None})
+    def destroy(
+        self
+    ) -> None:
+        self.btn_obj.destroy()
