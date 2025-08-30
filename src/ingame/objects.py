af6a48fbecc35cc2f586ff07566ced5368a67aac
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-08-30 10:59:03 -0400

    Update objects.py
    Add Text object

--- src/ingame/objects.py
+++ src/ingame/objects.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from typing import Optional, Any
+from .core import Screen
 
 class Button:
-    btn_obj: tk.Button
+    button_obj: tk.Button
 
     def __init__(
         self,
-        screen_obj: Optional[Any] = None,
+        screen_obj: Optional[Screen] = None,
         /,
         packargs: Optional[dict[Any, Optional[Any]]] = None,
         **kwargs: Any
@@ -17,9 +18,32 @@ class Button:
         if packargs is None:
             packargs = {}
 
-        self.btn_obj = tk.Button(screen_obj.root, **kwargs)
-        self.btn_obj.pack(**{k: v for k, v in packargs.items() if v is not None})
+        self.button_obj = tk.Button(screen_obj.root, **kwargs)
+        self.button_obj.pack(**{k: v for k, v in packargs.items() if v is not None})
     def destroy(
         self
     ) -> None:
-        self.btn_obj.destroy()
+        self.button_obj.destroy()
+
+class Text:
+    text_obj: tk.Label
+
+    def __init__(
+        self,
+        screen_obj: Optional[Screen] = None,
+        /,
+        packargs: Optional[dict[Any, Optional[Any]]] = None,
+        **kwargs: Any
+    ) -> None:
+        if screen_obj is None:
+            raise TypeError('Parameter "screen_obj" must be specified.')
+
+        if packargs is None:
+            packargs = {}
+
+        self.text_obj = tk.Label(screen_obj.root, **kwargs)
+        self.text_obj.pack(**{k: v for k, v in packargs.items() if v is not None})
+    def destroy(
+        self
+    ) -> None:
+        self.text_obj.destroy()
